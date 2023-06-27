9833fd04c4d98fe6630d1d34feb4430ad59349dc
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2023-06-27 15:57:11 +0200

    add all section need to put content now

--- application.js
+++ application.js
@@ -3,12 +3,75 @@
 const navLinks = document.querySelectorAll('.nav-link');
 let navbarIndex = 0;
 
-navLinks.forEach((link, index) => {
-  link.addEventListener('click', (event) => {
-    navLinks[navbarIndex].classList.remove('active');
-    event.target.classList.add('active');
-    navbarIndex = index;
+const navbar = (target) => {
+  navLinks[navbarIndex].classList.remove('active');
+  target.classList.add('active');
+}
+//* END
+
+//* LINKS DECLARATION
+
+const homenav = document.querySelectorAll('.nav-link')[0];
+
+const project = document.querySelector('.project-container');
+const projectnav = document.querySelectorAll('.nav-link')[1];
+
+const about = document.querySelector('.about-container');
+const aboutnav = document.querySelectorAll('.nav-link')[2];
+
+//* END
+
+//* SCROLL LINKS
+
+navLinks[0].addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
   });
 });
 
-//* NAVBAR END
+navLinks[1].addEventListener('click', () => {
+  project.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+navLinks[2].addEventListener('click', () => {
+  about.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+//* END
+
+window.addEventListener('scroll', () => {
+
+  //* HOME
+  if (window.scrollY > 0
+    && window.scrollY < homenav.scrollHeight + (window.innerHeight / 2)
+    && !homenav.classList.contains('active'))
+    {
+      navbar(homenav);
+      navbarIndex = 0;
+    }
+
+  //* PROJECT
+
+  if (window.scrollY > project.offsetTop - (window.innerHeight / 2)
+    && window.scrollY < project.offsetTop + project.scrollHeight + (window.innerHeight / 2)
+    && !projectnav.classList.contains('active'))
+    {
+      navbar(projectnav);
+      navbarIndex = 1;
+    }
+
+  //* ABOUT
+  if (window.scrollY > about.offsetTop - (window.innerHeight / 2)
+    && window.scrollY < about.offsetTop + about.scrollHeight + (window.innerHeight / 2)
+    && !aboutnav.classList.contains('active'))
+    {
+      navbar(aboutnav);
+      navbarIndex = 2;
+    }
+
+});
